92c6151aebbce84fb82957a579de7357cc331b92
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-10-11 09:10:24 -0300

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Oferta } from './../shared/oferta.model';
+import { OfertasService } from './../ofertas.service';
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas!: Oferta[]
+
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit(): void {
+    this.ofertas = this.ofertasService.getOfertas()
+    console.log(this.ofertas);
+  }
+
+}
