860dcb6c6577360988946784a341dfd71c6eba27
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-10-13 17:53:34 -0300

    search

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,17 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { OfertasService } from './../ofertas.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Oferta } from '../shared/oferta.model';
+// import { interval, Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService],
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
 
-  constructor(private route: ActivatedRoute) { }
+  // private tempoObservableSubscription!: Subscription
+  // private meuObservableTesteSubscription!: Subscription
+
+  public oferta!: Oferta;
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) {}
 
   ngOnInit(): void {
-    console.log('Id recuperado da rota:', this.route.snapshot.params['id'] );
+    // console.log('Id recuperado da rota:', this.route.snapshot.params['id']);
+    this.ofertasService
+      .getOfertaPorId(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta;
+      });
+
+    
+    // this.route.params.subscribe(
+    //     (parametro: any) => {console.log(parametro)}),
+    //     (erro: any) => console.log(erro),
+    //     () => console.log('processamento foi classificado como concluido');
+      
+
+    // let tempo = interval(2000);
+    // this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
+    //   console.log(intervalo);
+    // });
+
+    // //observable(observavel)
+    //   let meuObservableTeste = new Observable((observer: Observer<number>) => {
+    //     observer.next(1)
+    //     observer.next(3)
+    //     observer.complete()
+    //     observer.next(5)
+    //   })
+
+    // //observable(observador)
+    //   this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
+    //     (resultado: number) => console.log(resultado + 10),
+    //     (erro: string) => console.log(erro),
+    //     () => console.log('stream de enventos foi finalizada')
+    //   )
+  }
+
+  ngOnDestroy() {
+    // this.meuObservableTesteSubscription.unsubscribe()
+    // this.tempoObservableSubscription.unsubscribe()
   }
 
 }
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,15 +1,28 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas!: Observable<Oferta[]>
 
-  ngOnInit(): void {
+  constructor(private ofertasService: OfertasService) {}
+
+  ngOnInit(): void {}
+
+  public pesquisa(termoDaPesquisa: string): void {
+    this.ofertas =  this.ofertasService.pesquisaOfertas(termoDaPesquisa)
+    
+    this.ofertas.subscribe(
+      (ofertas: Oferta[]) => console.log(ofertas)
+    )
+    
   }
-
 }
